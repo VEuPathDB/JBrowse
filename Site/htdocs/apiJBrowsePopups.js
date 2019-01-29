b49301406a6d43cbc8c32c4e209ff1f45cec610f
John Brestelli <jbrestell@gmail.com>
2019-01-29 21:56:09 +0000

    add util for title case
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/JBrowse/trunk@94882 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/apiJBrowsePopups.js
+++ Site/htdocs/apiJBrowsePopups.js
@@ -26,6 +26,15 @@ function round(value, decimals) {
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
 
+
+function titleCase(str) {
+   var splitStr = str.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   return splitStr.join(' '); 
+}
+
 /****** Pop-up functions for various record types ******/
 
 // Gene title
