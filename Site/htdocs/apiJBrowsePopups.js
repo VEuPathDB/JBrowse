ba9b16ac67eb56f6a55718bc3d6821f02355c3e0
John Brestelli <jbrestell@gmail.com>
2019-02-13 21:54:14 +0000

    intron junctions details
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/JBrowse/trunk@95277 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/apiJBrowsePopups.js
+++ Site/htdocs/apiJBrowsePopups.js
@@ -96,6 +96,92 @@ function gene_title (tip, projectId, sourceId, chr, loc, soTerm, product, taxon,
 }
 
 
+function gsnapUnifiedIntronJunctionTitle (track, feature, featureDiv) {
+    var rows = new Array();
+    //arrays
+    var exps = feature.get('Exps');
+    var samples = feature.get('Samples');
+    var urs = feature.get('URS');
+    var isrpm = feature.get('ISRPM');
+    var nrs =  feature.get('NRS');
+    var percSamp = feature.get('PerMaxSample'); 
+    var isrCovRatio = feature.get('IsrCovRatio'); 
+    var isrAvgCovRatio = feature.get('IsrAvgCovRatio'); 
+    var normIsrCovRatio = feature.get('NormIsrCovRatio'); 
+    var normIsrAvgCovRatio = feature.get('NormIsrAvgCovRatio'); 
+    var isrpmExpRatio = feature.get('IsrpmExpRatio'); 
+    var isrpmAvgExpRatio = feature.get('IsrpmAvgExpRatio'); 
+
+    //attributes
+    var totalScore = feature.get('TotalScore'); 
+    var intronPercent = feature.get('IntronPercent'); 
+    var intronRatio = feature.get('IntronRatio'); 
+    var matchesGeneStrand = feature.get('MatchesGeneStrand'); 
+    var isReversed = feature.get('IsReversed'); 
+    var annotIntron = feature.get('AnnotatedIntron'); 
+    var gene_source_id = feature.get('GeneSourceId'); 
+
+    var start = feature.get("start");
+    var end = feature.get("end");
+
+    var exp_arr = exps.split('|');
+    var sample_arr = samples.split('|');
+    var ur_arr = urs.split('|');
+    var isrpm_arr = isrpm.split('|');
+    var percSamp_arr = percSamp.split('|');
+    var isrCovRatio_arr = isrCovRatio.split('|');
+    var isrAvgCovRatio_arr = isrAvgCovRatio.split('|');
+    var count = 0;
+    var html;
+    if(intronPercent) {
+        html = "<table><tr><th>Experiment</th><th>Sample</th><th>Unique</th><th>ISRPM</th><th>ISR/Cov</th><th>% MAI</th></tr>";
+    }
+    else {
+        html = "<table><tr><th>Experiment</th><th>Sample</th><th>Unique</th><th>ISRPM</th><th>ISR/AvgCov</th></tr>";
+    }
+
+    var maxRatio = [0,0,'sample here','experiment'];
+    var sumIsrpm = 0;
+
+    exp_arr.forEach(function(exp) {
+        var sa = sample_arr[count].split(',');
+        var ur = ur_arr[count].split(',');
+        var isrpm = isrpm_arr[count].split(',');
+        var rcs = isrCovRatio_arr[count].split(',');
+        var rct = isrAvgCovRatio_arr[count].split(',');
+        var ps = percSamp_arr[count].split(',');
+
+        var i;
+        for (i = 0; i < sa.length; i++) { 
+
+            if(Number(isrpm[i]) > Number(maxRatio[0])) maxRatio = [ isrpm[i], intronPercent ? rcs[i] : rct[i], sa[i], exp, intronPercent ? rcs[i] : rct[i] ];
+            sumIsrpm = sumIsrpm + Number(isrpm[i]);
+
+            if(i == 0) {
+                html = html + "<tr><td>"+ exp+ "</td><td>" + sa[i] + "</td><td>" + ur[i] + "</td><td>" + isrpm[i] + "</td>"; 
+            } else {
+                html = html + "<tr><td></td><td>" + sa[i] + "</td><td>" + ur[i] + "</td><td>" + isrpm[i] + "</td>"; 
+            }
+            if(intronPercent){
+                html = html + "<td>" + rcs[i] + "</td><td>" + ps[i] + "</td></tr>";
+            }else{
+                html = html + "<td>" + rct[i] + "</td></tr>";
+            }
+        }
+        count++;
+    });
+    html = html + "</table>";
+
+    rows.push(twoColRow('<b>Intron Location:</b>', "<b>" + start + " - " + end + " (" + (end - start + 1) + ' nt)</b>'));
+    rows.push(twoColRow('<b>Intron Spanning Reads (ISR):</b>', "<b>" + totalScore + "</b>" ));
+    rows.push(twoColRow('<b>ISR per million (ISRPM):</b>', "<b>" + round(sumIsrpm, 2) + "</b>" ));
+    if(intronPercent) rows.push(twoColRow('<b>Gene assignment:</b>', "<b>" + gene_source_id + (annotIntron === "Yes" ? " - annotated intron" : "") + "</b>"));
+    if(intronPercent) rows.push(twoColRow('<b>&nbsp;&nbsp;&nbsp;% of Most Abundant Intron (MAI):</b>', "<b>" + intronPercent + "</b>"));
+    rows.push(twoColRow('<b>Most abundant in:</b>', "<b>" + maxRatio[3] + " : " + maxRatio[2] + "</b>"));
+    rows.push(twoColRow('<b>&nbsp;&nbsp;&nbsp;ISRPM (ISR /' + (annotIntron === 'Yes' ? ' gene coverage)' : ' avg coverage)') + '</b>', "<b>" + maxRatio[0] + " (" + maxRatio[1] +")</b>"));
+
+    return table(rows) + html;
+ }
 
 
 
