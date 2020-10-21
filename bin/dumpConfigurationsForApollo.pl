f030ba25bfb2c6893577b77159a85093d59ab1c8
John Brestelli <jbrestel@upenn.edu>
2020-10-21 16:57:08 -0400

    cleaned up a bit and added options so that a subdomain can be specified as input website but stripped off in the configuration

--- bin/dumpConfigurationsForApollo.pl
+++ bin/dumpConfigurationsForApollo.pl
@@ -11,15 +11,14 @@ use File::Copy "cp";
 use Getopt::Long;
 use Data::Dumper;
 
-my ($help, $directory, @sites, $workflowDir);
-
-
-# /eupath/data/EuPathDB/workflows/VectorBase/47/data/agamPEST/makeAndMaskTopLevelGenome/topLevelGenomicSeqs.fasta
+my ($help, $directory, @websites, $workflowDir, $subdomain, $stripSubdomain);
 
 &GetOptions('help|h' => \$help,
             'workflow_dir=s' => \$workflowDir,
             'output_dir=s' => \$directory,
-            'website=s' => \@sites,
+            'website=s' => \@websites,
+            'subdomain=s' => \$subdomain,
+            'strip_subdomain' => \$stripSubdomain,
     );
 
 if($help) {
@@ -27,38 +26,52 @@ if($help) {
   exit;
 }
 
-foreach my $site (@sites) {
-  my $orgUrl = "$site/a/service/jbrowse/organismList";
+my %partialProjects;
+while(<DATA>) {
+  chomp;
+  my ($p, $o) = split(',', $_);
+  $partialProjects{$p}->{$o}=  1;
 
-  my $organisms = &getData($orgUrl);
+}
 
-  my $organismsFile = "$directory/organismList.json";
 
-  open(ORGS, ">$organismsFile") or die "Cannot open file $organismsFile for writing: $!";
-  print ORGS encode_json $organisms;
-  close ORGS;
+my $dataDir = "$directory/data";
+mkdir $dataDir;
+
+foreach my $website (@websites) {
+  print STDERR "Working on website: $website\n";
+
+  my ($sourceWebsite, $websiteForApolloConfig, $cleanSiteDir) = &websiteNames($website, $subdomain, $stripSubdomain);
+
+  my $websiteDir = "$directory/$cleanSiteDir";
+  mkdir $websiteDir; 
+
+  my $isPartialProject = &isPartialProject($cleanSiteDir, \%partialProjects);
+
+  my $organisms = &getOrganismListAndPrintJson($sourceWebsite, $websiteDir);
 
   foreach my $organism (@{$organisms->{organisms}}) {
     my $organismAbbrev = $organism->{ORGANISM_ABBREV};
 
-#    next unless $organismAbbrev eq 'agamPEST';
+    next if($isPartialProject && &skipOrganism($organismAbbrev, \%partialProjects));
+
     print STDERR "Preparing Configuration for Organism $organismAbbrev\n";
-    my $mainUrl = "$site/a/service/jbrowse/tracks/${organismAbbrev}/trackList.json";
+    my $mainUrl = "$sourceWebsite/a/service/jbrowse/tracks/${organismAbbrev}/trackList.json";
     my $main = &getData($mainUrl);
 
     # include the functions.conf
     push @{$main->{include}}, "/a/jbrowse/functions.conf";
 
-    # todo: only include this for annotated genomes
+    # TODO: only include this for annotated genomes
     push @{$main->{include}}, "/a/jbrowse/apollo_gene_tracks.conf";
 
-    my $organismDir = "$directory/$organismAbbrev";
-    my $seqDir = "$directory/$organismAbbrev/seq";
+    my $organismDir = "$dataDir/$organismAbbrev";
+    my $seqDir = "$dataDir/$organismAbbrev/seq";
 
     mkdir $organismDir;
     mkdir $seqDir;
 
-    my $refSeqsUrl = &redirect($site, $main->{refSeqs}, $organismAbbrev);
+    my $refSeqsUrl = &redirect($sourceWebsite, $main->{refSeqs}, $organismAbbrev);
     my $refSeqsJson = get($refSeqsUrl);
     my $refSeqsFile = "$seqDir/refSeqs.json";
 
@@ -66,21 +79,25 @@ foreach my $site (@sites) {
     print REFSEQ $refSeqsJson . "\n";
     close REFSEQ;
 
-    my $fastaFile = "$workflowDir/data/$organismAbbrev/makeAndMaskTopLevelGenome/topLevelGenomicSeqs.fasta";
-    unless(-e $fastaFile) {
-      die "file does not exist: $fastaFile";
-    }
+    # TODO: use webservice for srt to get the top level fasta seqs
+    # my $fastaFile = "$workflowDir/data/$organismAbbrev/makeAndMaskTopLevelGenome/topLevelGenomicSeqs.fasta";
+    # unless(-e $fastaFile) {
+    #   die "file does not exist: $fastaFile";
+    # }
 
-    my $fa = "$organismAbbrev.fa";
-    my $fai = "$organismAbbrev.fa.fai";
-    my $refSeqFasta = "$organismDir/seq/$fa";
+     my $fa = "$organismAbbrev.fa";
+     my $fai = "$organismAbbrev.fa.fai";
+     my $refSeqFasta = "$organismDir/seq/$fa";
 
-    cp($fastaFile, $refSeqFasta);
+    # TODO:
+    # # will Bob's Server have samtools installed?  
+    # # can we use docker or singularity to index the fasta? (ask BobB)
 
-    system("samtools faidx $refSeqFasta") == 0
-        or die "samtools command failed: $?";
+    # cp($fastaFile, $refSeqFasta);
+    # system("samtools faidx $refSeqFasta") == 0
+    #     or die "samtools command failed: $?";
 
-    $main->{refSeqs} = "seq/$fai";
+     $main->{refSeqs} = "seq/$fai";
 
     $main->{tracks} = [
       {"category" => "Sequence Analysis",  
@@ -95,7 +112,7 @@ foreach my $site (@sites) {
       }
         ];
 
-    $main->{names}->{url} = &redirect($site, $main->{names}->{url}, $organismAbbrev);
+    $main->{names}->{url} = &redirect($websiteForApolloConfig, $main->{names}->{url}, $organismAbbrev);
 
     # weed out user datasets
     @{$main->{include}} = grep {!/user-datasets-jbrowse/} @{$main->{include}};
@@ -103,7 +120,7 @@ foreach my $site (@sites) {
     for(my $i = 0; $i < @{$main->{include}}; $i++) {
       my $includeUrl = $main->{include}->[$i];
 
-      $includeUrl = &redirect($site, $includeUrl, $organismAbbrev);
+      $includeUrl = &redirect($sourceWebsite, $includeUrl, $organismAbbrev);
 
       if($includeUrl =~ /rnaseqJunctions/) {
         $includeUrl = $includeUrl .  "?isApollo=1";
@@ -111,7 +128,7 @@ foreach my $site (@sites) {
 
       my $tracksJson = get($includeUrl);
       die "Couldn't get $includeUrl" unless defined $tracksJson;
-      $tracksJson =~ s/\/a\//$site\/a\//g;
+      $tracksJson =~ s/\/a\//$websiteForApolloConfig\/a\//g;
 
       my ($fileName) = $includeUrl =~ /jbrowse\/(.+)\/$organismAbbrev/; 
       $fileName = "$fileName.json";
@@ -148,6 +165,71 @@ foreach my $site (@sites) {
 }
 
 
+sub getOrganismListAndPrintJson {
+  my ($site, $websiteDir) = @_;
+
+  my $orgUrl = "$site/a/service/jbrowse/organismList";
+  my $organismsFile = "$websiteDir/organismList.json";
+
+  my $organisms = &getData($orgUrl);
+
+  open(ORGS, ">$organismsFile") or die "Cannot open file $organismsFile for writing: $!";
+  print ORGS encode_json $organisms;
+  close ORGS;
+
+  return $organisms;
+}
+
+
+sub websiteNames {
+  my ($site, $subdomain, $stripSubdomain) = @_;
+
+  if($site =~ /(https?:\/\/)?(.+).org\/?/) {
+    my $siteNoSub = "https://$2.org";
+
+    my $sourceSite = $subdomain ? "https://$subdomain.$2.org" : $siteNoSub;
+    my $siteForApolloConfig = $stripSubdomain ? $siteNoSub : $sourceSite;
+
+    my $cleanSource = $subdomain ? "$subdomain.$2.org" : "$2.org";
+    my $cleanSiteDir = $stripSubdomain ? "$2.org" : $cleanSource;
+    
+    print STDERR "Configs will be generated from:  $sourceSite\n";
+    print STDERR "The website referenced in output configs will be:  $siteForApolloConfig\n";
+    print STDERR "The directory name for this website is:  $cleanSiteDir\n";
+
+    return ($sourceSite, $siteForApolloConfig, $cleanSiteDir);
+  }
+
+  die "Did not recognize veupathdb website: $site";
+}
+
+
+sub skipOrganism {
+  my ($organismAbbrev, $partialProjects) = @_;
+
+  my $found = 0;
+  foreach my $p (keys %$partialProjects) {
+    $found = 1 if($partialProjects->{$p}->{$organismAbbrev});
+  }
+
+  print "$organismAbbrev was found\n" if $found;
+
+  return !$found;
+}
+
+sub isPartialProject {
+  my ($site, $partialProjects) = @_;
+
+  foreach(keys %$partialProjects) {
+    my $lcProjectName = lc($_);
+    if( $site =~ /$lcProjectName/) {
+      print STDERR "A partial List of organism configurations will be dumped for $site\n";
+      return 1 ;
+    }
+  }
+
+  return 0;
+}
 
 sub redirect {
   my ($site, $url, $organismAbbrev) = @_;
@@ -175,3 +257,43 @@ sub getData {
 sub usage {
   print STDERR "dumpConfigurationsForApollo.pl --workflow_dir=<DIR> --output_dir=<DIR>  --website=\@s\n";
 }
+
+1; 
+
+
+__DATA__
+TriTrypDB,lbraMHOMBR75M2904
+TriTrypDB,ldonBPK282A1
+TriTrypDB,linfJPCM5
+TriTrypDB,lmajFriedlin
+TriTrypDB,lmexMHOMGT2001U1103
+TriTrypDB,tbrugambienseDAL972
+TriTrypDB,tbruLister427_2018
+TriTrypDB,tconIL3000
+TriTrypDB,tcruCLBrenerEsmeraldo-like
+TriTrypDB,tcruCLBrenerNon-Esmeraldo-like
+TriTrypDB,tcruCLBrener
+TriTrypDB,tvivY486
+CryptoDB,cparIOWA-ATCC
+GiardiaDB,gassAWB
+FungiDB,afumAf293
+FungiDB,anidFGSCA4
+FungiDB,anigCBS513-88
+FungiDB,aoryRIB40
+FungiDB,bcinB05-10
+FungiDB,cimmRS
+FungiDB,cposSilveira
+FungiDB,cneoH99
+FungiDB,cneoJEC21
+FungiDB,fgraPH-1
+FungiDB,hcapNAm1
+FungiDB,mory70-15
+FungiDB,mgloCBS7966
+FungiDB,ncraOR74A
+FungiDB,pjirSE8
+FungiDB,treeQM6a
+FungiDB,umay521
+FungiDB,cgatWM276
+FungiDB,calbSC5314
+FungiDB,foxy4287
+FungiDB,caurB8441
