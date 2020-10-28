69124bb9df524929509a29e34d332c9e7a88bf21
Paul Wilkinson <pwilkinson@elm.pcbi.upenn.edu>
2020-10-28 10:44:48 -0400

    First commit of makeGroovyAddOrganismCommands.pl - checks for new organism and version

--- bin/makeGroovyAddOrganismCommands.pl
+++ bin/makeGroovyAddOrganismCommands.pl
@@ -4,18 +4,34 @@ use strict;
 
 use JSON;
 use Data::Dumper;
+use CBIL::Util::Utils;
+
 
 my $filename = "organismList.json";
 my $file_content = do{local(@ARGV,$/)=$filename;<>};
 
-
 my $organisms = decode_json $file_content;
+print "Extracting current organisms in Apollo\n";
+my $cmd = "curl -X POST -F 'username=api\@local.host' -F 'password=GFERsVNiX5BQ09uN' -L https://apollo-api\.veupathdb\.org/organism/findAllOrganisms | jq  '\.[]\.commonName'";
+my @currentOrganisms = &runCmd($cmd);
+chomp @currentOrganisms;
 
 foreach (@{$organisms->{organisms}}) {
     my $fullName = $_->{NAME};
     my $organismAbbrev = $_->{ORGANISM_ABBREV};
     my $annotationVersion = $_->{ANNOTATION_VERSION};
 
-    print "groovy add_organism.groovy -name '$fullName [$annotationVersion]' -url https://apollo.apidb.org/ -directory /data/apollo_data/${organismAbbrev} -username 'admin@local.host' -password  \$APOLLO_ADMIN_PASSWORD\n";
+my $checkOrganism = $fullName." \[".$annotationVersion."\]";
+
+if ( grep( /\Q$checkOrganism/, @currentOrganisms)){
+	next;
+}
+elsif( grep( /\Q$fullName/, @currentOrganisms) ){
+	print "groovy add_organism.groovy -name '$fullName \[$annotationVersion\]' -url https://apollo.apidb.org/ -directory /data/apollo_data/${organismAbbrev} -username 'admin\@local.host' -password  \$APOLLO_ADMIN_PASSWORD\n";
+}
+else {
+	print "groovy add_organism.groovy -name '$fullName \[$annotationVersion\]' -url https://apollo.apidb.org/ -directory /data/apollo_data/${organismAbbrev} -username 'admin\@local.host' -password  \$APOLLO_ADMIN_PASSWORD\n";
+}
+
 }
 
