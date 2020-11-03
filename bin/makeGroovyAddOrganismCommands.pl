563c669533d86ee52b26bbdbd4b504d1906978d7
Paul Wilkinson <pwilkinson@elm.pcbi.upenn.edu>
2020-11-03 15:01:12 -0500

    Second  commit of makeGroovyAddOrganismCommands.pl - fixed update organism with arrow command

--- bin/makeGroovyAddOrganismCommands.pl
+++ bin/makeGroovyAddOrganismCommands.pl
@@ -13,7 +13,7 @@ my $file_content = do{local(@ARGV,$/)=$filename;<>};
 my $organisms = decode_json $file_content;
 print "Extracting current organisms in Apollo\n";
 my $cmd = "curl -X POST -F 'username=api\@local.host' -F 'password=GFERsVNiX5BQ09uN' -L https://apollo-api\.veupathdb\.org/organism/findAllOrganisms | jq  '\.[]\.commonName'";
-my @currentOrganisms = &runCmd($cmd);
+my @currentOrganisms = split /"/, runCmd($cmd);
 chomp @currentOrganisms;
 
 foreach (@{$organisms->{organisms}}) {
@@ -27,7 +27,16 @@ if ( grep( /\Q$checkOrganism/, @currentOrganisms)){
 	next;
 }
 elsif( grep( /\Q$fullName/, @currentOrganisms) ){
-	print "groovy add_organism.groovy -name '$fullName \[$annotationVersion\]' -url https://apollo.apidb.org/ -directory /data/apollo_data/${organismAbbrev} -username 'admin\@local.host' -password  \$APOLLO_ADMIN_PASSWORD\n";
+
+	my ($oldCommonName) = grep {/$fullName/} @currentOrganisms ;
+        my $commonName = "$fullName \[$annotationVersion\]";
+	my $getIdCmd = "arrow organisms get_organisms --common_name \"".$oldCommonName."\" \| jq -r '\.\[\]\.id, \.\[\]\.directory'";
+	my @results = `$getIdCmd`;
+	chomp @results;
+	my $organismId = $results[0];
+ 	my $directory = $results[1];
+	print "arrow organisms update_organism ".$organismId." \"".$commonName."\" \"".$directory."\" \n";
+#	print "groovy add_organism.groovy -name '$fullName \[$annotationVersion\]' -url https://apollo.apidb.org/ -directory /data/apollo_data/${organismAbbrev} -username 'admin\@local.host' -password  \$APOLLO_ADMIN_PASSWORD\n";
 }
 else {
 	print "groovy add_organism.groovy -name '$fullName \[$annotationVersion\]' -url https://apollo.apidb.org/ -directory /data/apollo_data/${organismAbbrev} -username 'admin\@local.host' -password  \$APOLLO_ADMIN_PASSWORD\n";
